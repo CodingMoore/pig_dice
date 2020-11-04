3967a9e19b274218e5d6c7023ee2a8696f4c1b88
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-04 14:45:41 -0800

    fix bug in scoring values
    Co-authored-by: Randel Moore <randel.c.moore@gmail.com>

--- js/README.md
+++ js/README.md
@@ -1,4 +1,4 @@
-| Description Roll();
+| Description p1Roll();
 
 Test: "It will generate a random number as output between 1-6"
 Expect: ((p1Roll).click).toEqual("1-6");
@@ -15,10 +15,35 @@ Expect ((p1Roll).click).toEqual(total = subtotal + total);
 Test: "It will change subtotal to an empty array when current roll is equal to 1"
 Expect: (((p1Roll).click)=1)).toEqual(array1=[]);
 
-| Description Hold();
+| Description p1Hold();
 
 Test: "It will add total to score"
 Expect: ((p1Hold).click).toEqual(score = score + total);
 
 Test: "It will hide buttons for player one after clicking hold, show player two buttons and vice versa"
 Expect: ((p1Hold).click).toEqual(show p2Roll and P2Hold buttons) and (hide p1Roll and p1Hold buttons);
+
+| Description p2Roll();
+
+Test: "It will generate a random number as output between 1-6"
+Expect: ((p2Roll).click).toEqual("1-6");
+
+Test: "It will store output number as element in array1"
+Expect: ((p2Roll).click).toEqual([3, 2, 5]);
+
+Test: "It will push the sum of elements in array1 to subtotal"
+Expect: ((p2Roll).click).toEqual(array1 = [3, 2, 5]) subtotal = "10"
+
+Test: "It will add the subtotal to total"
+Expect ((p2Roll).click).toEqual(total = subtotal + total);
+
+Test: "It will change subtotal to an empty array when current roll is equal to 1"
+Expect: (((p2Roll).click)=1)).toEqual(array1=[]);
+
+| Description p2Hold();
+
+Test: "It will add total to score"
+Expect: ((p2Hold).click).toEqual(score = score + total);
+
+Test: "It will hide buttons for player one after clicking hold, show player two buttons and vice versa"
+Expect: ((p2Hold).click).toEqual(show p1Roll and P1Hold buttons) and (hide p1Roll and p1Hold buttons);
--- js/scripts-test.js
+++ js/scripts-test.js
@@ -1,19 +1,25 @@
 //Business Logic
 p1RollArray = [];
-score = 0;
-
+p2RollArray = [];
+score1 = 0;
+score2 = 0;
 
 function RollSum(array) {
-sum = 0;
-for (let i = 0; i < array.length; i++) {
-  sum += array[i];
+  sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
   return sum
 }
 
-function Score(sum) {
-  score += sum;
-  return score
+function Score1(sum) {
+  score1 += sum;
+  return score1
+}
+
+function Score2(sum) {
+  score2 += sum;
+  return score2
 }
 
 //UI Logic
@@ -21,6 +27,10 @@ $(document).ready(function() {
   $("#p1TotalOutput").text("0");
   $("#p1RollOutput").text("0");
   $("#p1ScoreOutput").text("0");
+  $("#p2TotalOutput").text("0");
+  $("#p2RollOutput").text("0");
+  $("#p2ScoreOutput").text("0");
+
   $("#p1Roll").click(function(event) {
     event.preventDefault();
     const p1Roll = (Math.floor(Math.random() * 6) + 1)
@@ -37,13 +47,42 @@ $(document).ready(function() {
   $("#p1Hold").click(function(event) {
     event.preventDefault();
     let p1Sum = RollSum(p1RollArray);
-    $("#p1ScoreOutput").text(Score(p1Sum));
-    $("#p1Roll").toggle();
-    $("#p1Hold").toggle();
-    $("#p2Roll").toggle();
-    $("#p2Hold").toggle();
+    $("#p1ScoreOutput").text(Score1(p1Sum));
+    $("#p1Roll").hide();
+    $("#p1Hold").hide();
+    $("#p2Roll").show();
+    $("#p2Hold").show();
     $("#p1TotalOutput").text("0");
     $("#p1RollOutput").text("0");
+    console.log(p1RollArray);
+    console.log(p1Sum);
+  });
+
+  $("#p2Roll").click(function(event) {
+    event.preventDefault();
+    const p2Roll = (Math.floor(Math.random() * 6) + 1)
+    if (p2Roll === 1) {
+      p2RollArray = [0];
+      $("#p2RollOutput").text(p2Roll);
+    } else {
+      p2RollArray.push(p2Roll);
+    }
+    let p2Sum = RollSum(p2RollArray);
+    $("#p2RollOutput").text(p2Roll);
+    $("#p2TotalOutput").text(p2Sum);
+  });
+  $("#p2Hold").click(function(event) {
+    event.preventDefault();
+    let p2Sum = RollSum(p2RollArray);
+    $("#p2ScoreOutput").text(Score2(p2Sum));
+    $("#p2Roll").hide();
+    $("#p2Hold").hide();
+    $("#p1Roll").show();
+    $("#p1Hold").show();
+    $("#p2TotalOutput").text("0");
+    $("#p2RollOutput").text("0");
+    console.log(p2RollArray);
+    console.log(p2Sum);
   });
   
 });
