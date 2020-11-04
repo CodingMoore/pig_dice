3411fb90466ad6477814d7ff87e95b38f7c5042b
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-04 10:45:05 -0800

    add initial HTML, CSS, JS, and README files
    Co-authored-by: Randel Moore <randel.c.moore@gmail.com>

---
+++ js/README.md
@@ -0,0 +1,5 @@
+| Description Roll();
+
+|Test: "It will generate a random number as output between 1-6"
+Expect: ((p1Roll).click).toEqual("1-6");
+
---
+++ js/scripts-test.js
@@ -0,0 +1,16 @@
+//UI Logic
+$(document).ready(function() {
+  $("#p1Roll").click(function(event) {
+    event.preventDefault();
+    const p1Roll = (Math.floor(Math.random() * 6) + 1)
+    alert(p1Roll);
+  });
+
+
+
+
+
+
+
+
+});
